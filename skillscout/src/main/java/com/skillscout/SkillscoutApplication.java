3d9cc55ab59d00ac20e456fc72e1f570f355837d
HassanAlwakeel1 <hassanalwakeel911@gmail.com>
2025-01-09 18:07:10 +0200

    Initial commit

---
+++ skillscout/src/main/java/com/skillscout/SkillscoutApplication.java
@@ -0,0 +1,13 @@
+package com.skillscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillscoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillscoutApplication.class, args);
+	}
+
+}
